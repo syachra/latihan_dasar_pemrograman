9a990c3a90cabd2e76279c84d48d4f53eed852b3
syachra <114629791+syachra@users.noreply.github.com>
2022-09-28 21:27:08 -0700

    Biodata

---
+++ aplikasiBiodata.py
@@ -0,0 +1,24 @@
+# aplikasi biodata create by syachra indyra puteri
+
+#  memasukan nama
+nama = input("Masukan nama anda")
+#output nama
+print("nama anda adalah: ", nama)
+#input kelas
+kelas=input("masukan kelas")
+#output kelas
+print("kelas anda adalah; ", kelas)
+#input nim  
+nim = input("masukin nim!")
+#output nim
+print("nim anda adalah; ", nim)
+#input prodi
+prodi = input ("masukan prodi!")
+#output prodi
+print("anda dari prodi; ", prodi)
+#input tanggal lahir
+tanggal_lahir = input("masukan tanggal lahir")
+#output tanggal lahir
+print("tanggal lahir anda adalah;", tanggal_lahir)
+
+
